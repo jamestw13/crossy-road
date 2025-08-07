0e9c6e4aec75cf122a37629f8f26afb26d5fb988
TJ James <jamestw13@gmail.com>
2025-08-07 16:32:14 -0500

    removed fake board

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -12,13 +12,6 @@ export function Result() {
     <div id="result-container">
       <div id="result">
         <h1>Game Over</h1>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-          <strong>12874 KELSY</strong>
-          <strong>12870 TBLAD</strong>
-          <strong>12314 SEE-J</strong>
-          <strong> 9172 SHOKY</strong>
-          <strong> 7280 E V A</strong>
-        </div>
 
         <p>Your score: {score}</p>
         <button onClick={reset}>Retry</button>
