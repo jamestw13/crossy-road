c48507f98517842f2056fe263080684411602ef1
TJ James <jamestw13@gmail.com>
2025-08-07 16:26:35 -0500

    Added fake scoreboard

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -12,6 +12,14 @@ export function Result() {
     <div id="result-container">
       <div id="result">
         <h1>Game Over</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+          <strong>12874 KELSY</strong>
+          <strong>12870 TBLAD</strong>
+          <strong>12314 SEE-J</strong>
+          <strong> 7280 E V A</strong>
+          <strong> 2873 AAAAA</strong>
+        </div>
+
         <p>Your score: {score}</p>
         <button onClick={reset}>Retry</button>
       </div>
